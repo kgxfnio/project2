612792b8ecf977d70be85048438f3cfbecf9fe6e
kgxfnio <95566842+kgxfnio@users.noreply.github.com>
2022-07-22 20:32:23 +0300

    Add files via upload

--- projecto/js.js
+++ projecto/js.js
@@ -1,13 +1,19 @@
 function Display(){
-    if(document.getElementById('main-container').style.display == 'none'){
-        document.getElementById('main-container').style.display = 'flex'
-        document.getElementById('recount').style.display = 'inline'
-        document.getElementById('unhide').style.display = 'none'
+    if(document.getElementById('main-container').style.display == 'none' || document.getElementById('main-container').style.display == ''){
+        document.getElementById('main-container').style.display = 'flex';
+        document.getElementById('recount').style.display = 'inline';
+        document.getElementById('unhide').style.display = 'none';
+        document.getElementById('result').innerText = '';
+        document.querySelector('body').style.backgroundColor = 'rgb(35, 35, 35)';
+        document.querySelector('body').style.color = 'whitesmoke';
     }
     else{
         document.getElementById('main-container').style.display = 'none'
         document.getElementById('recount').style.display = 'none'
         document.getElementById('unhide').style.display = 'block'
+        document.getElementById('result').innerText = ''
+        document.querySelector('body').style.backgroundColor = 'white'
+        document.querySelector('body').style.color = 'black'
     }
 }
 function Calculate(){
@@ -20,5 +26,6 @@ function Calculate(){
         RUB:74.36,
         GBP:0.75,
     }
-    document.getElementById('result').innerText = list[`${currency}`]*amount;
+    let result = list[`${currency}`]*amount;
+    document.getElementById('result').innerText = `${amount} USD equals ${result} ${document.getElementById('currency-list').value}`;
 }
